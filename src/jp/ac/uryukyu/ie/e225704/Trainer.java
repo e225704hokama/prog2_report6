bcee77947636599bfc8af1c53ff6d1fb35266995
Hokama Reo <e225704@ie.u-ryukyu.ac.jp>
2023-01-22 20:47:45 +0900

    てもち導入

--- src/jp/ac/uryukyu/ie/e225704/Trainer.java
+++ src/jp/ac/uryukyu/ie/e225704/Trainer.java
@@ -1,8 +1,10 @@
 package jp.ac.uryukyu.ie.e225704;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Trainer {
+    private ArrayList<Pokemon> party = new ArrayList<Pokemon>(6);
 
     public void summary(Pokemon pokemon){ //種族名、レベル、実数値などのステータスを表示するメソッド
         System.out.println(pokemon.getterName() + "　" + pokemon.getterPrecious() + "　　Lv." + pokemon.getterLevel());
@@ -15,6 +17,28 @@ public class Trainer {
             System.out.println(statuss);
         }
         
+    }
+
+    private Pokemon partySelect(){
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        System.out.println("\nてもちポケモン");
+        for(var member : this.party){
+            System.out.println(i + ". " + member.getterName() + "　Lv." + member.getterLevel());
+            i ++;
+        }
+        while(true){
+            int number = scanner.nextInt();
+
+            if(number >=0 && number <= i){
+                System.out.println("\nいけ、 " + this.party.get(number).getterName() + "！\n");
+                return this.party.get(number);
+            }
+        }
+        
+        
+        
+
     }
 
     public void newGame(){
@@ -42,10 +66,11 @@ public class Trainer {
                     break;
                 }
                 System.out.println(p1.getterName() + "に決めた！\n");
+                party.add(p1);
                 start = false;
             }
             System.out.println("何をしようか？");
-            System.out.println("たたかう＝b、つよさをみる＝s、しんかさせる＝e、終了する＝f");
+            System.out.println("たたかう＝b、つよさをみる＝s、しんかさせる＝e、別のポケモンを育てる＝c、手持ちを見る＝p、終了する＝f");
             String order = scanner.next();
 
             switch(order){
@@ -57,6 +82,18 @@ public class Trainer {
                 break;
                 case "e" :
                 p1 = p1.evolution();
+                break;
+                case "c" :
+                if(party.size() < 6){
+                    System.out.println(p1.getterName() + "は手持ちに戻した。\n");
+                    start = true;
+                }
+                else{
+                    System.out.println("これ以上は育てるのは身がもたない．．．");
+                }
+                break;
+                case "p" :
+                p1 = this.partySelect();
                 break;
                 case "f" :
                 con = false;
